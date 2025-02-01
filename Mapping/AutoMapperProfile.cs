2f198b4fbb2d281f5260a8ed1376143121c4e3f6
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-01 17:40:28 +0200

    add mapping file

---
+++ Mapping/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using Learntendo_backend.Dtos;
+using Learntendo_backend.Models;
+using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Learntendo_backend.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Subject, SubjectDto>();
+
+            CreateMap<SubjectDto, Subject>();
+        }
+    }
+
+}
