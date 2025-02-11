7180d87c23141032e6ea944f797225df13c3d8a1
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-11 17:22:23 +0200

    add Exam mapping

--- Mapping/AutoMapperProfile.cs
+++ Mapping/AutoMapperProfile.cs
@@ -2,6 +2,8 @@
 using Learntendo_backend.Models;
 using AutoMapper;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using Learntendo_backend.Dtos.Learntendo_backend.DTOs;
+using Newtonsoft.Json;
 
 namespace Learntendo_backend.Mapping
 {
@@ -12,6 +14,18 @@ namespace Learntendo_backend.Mapping
             CreateMap<Subject, SubjectDto>();
 
             CreateMap<SubjectDto, Subject>();
+
+            CreateMap<ExamDto, Exam>()
+            .ForMember(dest => dest.McqQuestionsData, opt => opt.MapFrom(src =>
+                src.McqQuestionsData != null ? JsonConvert.SerializeObject(src.McqQuestionsData) : null))
+            .ForMember(dest => dest.TfQuestionsData, opt => opt.MapFrom(src =>
+                src.TfQuestionsData != null ? JsonConvert.SerializeObject(src.TfQuestionsData) : null));
+
+            CreateMap<Exam, ExamDto>()
+                .ForMember(dest => dest.McqQuestionsData, opt => opt.MapFrom(src =>
+                    src.McqQuestionsData != null ? JsonConvert.DeserializeObject<List<McqQuestionDto>>(src.McqQuestionsData) : null))
+                .ForMember(dest => dest.TfQuestionsData, opt => opt.MapFrom(src =>
+                    src.TfQuestionsData != null ? JsonConvert.DeserializeObject<List<TfQuestionDto>>(src.TfQuestionsData) : null));
         }
     }
 
