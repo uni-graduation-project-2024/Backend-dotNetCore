0a64e89f63213d88f14fd534ce65cfff6aeff0e4
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-11 20:07:37 +0200

    add update in subject table

--- Controllers/ExamController.cs
+++ Controllers/ExamController.cs
@@ -12,17 +12,21 @@ namespace Learntendo_backend.Controllers
     public class ExamController : Controller
     {
         private readonly IDataRepository<Exam> _ExamRepo;
+        private readonly IDataRepository<Subject> _subjectRepo;
         private readonly IMapper _map;
-
-        public ExamController(IDataRepository<Exam> examRepo, IMapper map)
+        public ExamController(IDataRepository<Subject> subjectRepo, IDataRepository<Exam> examRepo, IMapper map)
         {
             _ExamRepo = examRepo;
+            _subjectRepo = subjectRepo;
             _map = map;
         }
+
+ 
         [HttpPost]
         public async Task<IActionResult> CreateExam([FromBody] ExamDto examDto)
         {
             var exam = _map.Map<Exam>(examDto);
+            
             if (examDto.QuestionType == "MCQ")
             {
                 exam.TfQuestionsData = null;
@@ -30,8 +34,14 @@ namespace Learntendo_backend.Controllers
             if (examDto.QuestionType == "TF")
             {
                 exam.McqQuestionsData = null;
-            }
+            }            
             await _ExamRepo.AddFun(exam);
+            var subject = await _subjectRepo.GetByIdFun(exam.SubjectId);
+            subject.NumExams+=1;
+            subject.TotalQuestions += exam.NumQuestions;
+            await _subjectRepo.UpdateFun(subject);
+
+
             return CreatedAtAction(nameof(GetExamById), new { id = exam.ExamId }, exam);
             //return Ok("created Successfully");
         }
@@ -62,8 +72,24 @@ namespace Learntendo_backend.Controllers
             {
                 return NotFound($"Exam with {id} not found");
             }
+
             await _ExamRepo.DeleteFun(id);
-            return Ok("Deleted Successfully");
+            var subject = await _subjectRepo.GetByIdFun(exam.SubjectId);
+            subject.NumExams -= 1;
+            subject.TotalQuestions -= exam.NumQuestions;
+            await _subjectRepo.UpdateFun(subject);
+            return Ok("Exam Deleted Successfully");
         }
+        //[HttpPatch("{id}")]
+        //public async Task<IActionResult> MoveExamtoAnotherSubject(int id)
+        //{
+        //    var exam = await _ExamRepo.GetByIdFun(id);
+        //    if (exam == null)
+        //    {
+        //        return NotFound($"Exam with {id} not found");
+        //    }
+        //    await _ExamRepo.UpdateFun(Exam);
+        //    return Ok("Exam Updated Successfully");
+        //}
     }
 }
