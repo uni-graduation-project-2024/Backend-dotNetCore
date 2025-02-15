8cc3ac6c3fe84c4eecf9893e021941c64a4af856
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-15 22:45:34 +0200

    Add servics folser ,  add ResetDailyChallenges Function

---
+++ Services/DailyResetService.cs
@@ -0,0 +1,36 @@
+﻿using Learntendo_backend.Data;
+using Learntendo_backend.Models;
+
+namespace Learntendo_backend.Services
+{
+    public class DailyResetService
+    {
+        private readonly DataContext _db;
+        
+
+
+        public DailyResetService(DataContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public void ResetDailyChallenges()
+        {
+            var users = _db.User.ToList();
+
+            foreach (var user in users)
+            {
+                user.CompleteDailyChallenge = false;
+                user.DateCompleteDailyChallenge = null;
+                user.DailyXp = 0;
+                user.NumQuestionSolToday = 0;
+            }
+
+            _db.SaveChanges();
+        }
+    }
+}
+
+    
