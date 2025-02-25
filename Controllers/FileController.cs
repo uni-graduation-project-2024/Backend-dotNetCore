278b2256484edc354414059551a7bd7e5623492b
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-25 21:23:30 +0200

    create file controller

---
+++ Controllers/FileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learntendo_backend.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
