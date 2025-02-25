ce2516de13dcee51743662d1c392a4e619656074
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-25 21:49:02 +0200

    Add notmapped attribute

--- Models/File.cs
+++ Models/File.cs
@@ -1,9 +1,14 @@
-﻿namespace Learntendo_backend.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Learntendo_backend.Models
 {
     public class File
     {
         public int FileId { get; set; }
         public string FilePath { get; set; }
         public string FileName { get; set; }
+
+        [NotMapped]
+        public IFormFile FileUp { get; set; }
     }
 }
