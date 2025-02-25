85da440000093d0a46dba26e2ef350356ecada33
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-25 21:24:01 +0200

    add file model

---
+++ Models/File.cs
@@ -0,0 +1,9 @@
+﻿namespace Learntendo_backend.Models
+{
+    public class File
+    {
+        public int FileId { get; set; }
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+    }
+}
