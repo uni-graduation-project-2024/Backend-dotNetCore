f90f7996638e842741c47f47822b3521e198d5ed
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-25 23:00:47 +0200

    edit name

---
+++ Models/Files.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Learntendo_backend.Models
+{
+    public class Files
+    {
+        [Key]
+        public int FileId { get; set; }
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+
+        [NotMapped]
+        public IFormFile FileUp { get; set; }
+    }
+}
