ba03ebb902f3d5b5e6989331ba2f1aabafd82e25
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-28 19:46:55 +0200

    add creareddate

--- Models/Files.cs
+++ Models/Files.cs
@@ -9,6 +9,7 @@ namespace Learntendo_backend.Models
         public int FileId { get; set; }
         public string FilePath { get; set; }
         public string FileName { get; set; }
+        public DateTime CreatedDate { get; set; }
 
         [NotMapped]
         public IFormFile FileUp { get; set; }
