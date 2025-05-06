b29abfa4712de6bb997897ad6ccc2ac3b975efe0
Radwa Khaled <radwaabouzed@gmail.com>
2025-05-06 20:10:32 +0300

    add nullable

--- Models/Files.cs
+++ Models/Files.cs
@@ -7,7 +7,7 @@ namespace Learntendo_backend.Models
     public class Files
     {
         [Key]
-        public int FileId { get; set; }
+        public int? FileId { get; set; }
         public string FilePath { get; set; }
         public string FileName { get; set; }
         public DateTime CreatedDate { get; set; }
