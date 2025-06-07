c7115e26817b9da5692ada29cf45947792e3f857
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-08 02:46:03 +0300

    add user provider

---
+++ Hubs/NameUserIdProvider.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace Learntendo_backend.Hubs
+{
+    public class NameUserIdProvider : IUserIdProvider
+    {
+        public string GetUserId(HubConnectionContext connection)
+        {
+            return connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
