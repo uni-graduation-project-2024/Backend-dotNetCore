19a2c470633c019e05b42858eb64c138b30fbb84
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-08 02:46:25 +0300

    add chat class

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Learntendo_backend.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+    }
+
+}
