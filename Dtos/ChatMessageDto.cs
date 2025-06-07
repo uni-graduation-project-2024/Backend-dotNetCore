d4fbdf709b5ff16a46ad719d2f8aa90c8617c644
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-08 02:45:23 +0300

    add chat dto

---
+++ Dtos/ChatMessageDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learntendo_backend.Dtos
+{
+    public class ChatMessageDto
+    {
+        [Required]
+        public string SenderId { get; set; }
+
+        [Required]
+        public string ReceiverId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
