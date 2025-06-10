f07776cdce1f31e4087a8f1dde3c3678a429ba3b
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-10 17:32:27 +0300

    add service

---
+++ Services/LeaderboardService.cs
@@ -0,0 +1,22 @@
+﻿using Learntendo_backend.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Learntendo_backend.Services
+{
+    public class LeaderboardService
+    {
+        private readonly IHubContext<LeaderboardHub> _hubContext;
+
+        public LeaderboardService(IHubContext<LeaderboardHub> hubContext)
+        {
+
+
+            _hubContext = hubContext;
+        }
+
+        public async Task NotifyLeaderboardUpdate()
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveLeaderboardUpdate");
+        }
+    }
+}
