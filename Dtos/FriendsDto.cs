60c4e437532fdd661da5f73eb7190611b00fcd77
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-10 04:48:50 +0300

    add friend dto

---
+++ Dtos/FriendsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Learntendo_backend.Dtos
+{
+    public class FriendsDto
+    {
+        public int RequestId { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
