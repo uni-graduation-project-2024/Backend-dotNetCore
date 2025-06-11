804742658844e9098c3b6ab991d866df490c462a
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-11 18:37:28 +0300

    update

--- Dtos/ChatMessageDto.cs
+++ Dtos/ChatMessageDto.cs
@@ -4,13 +4,14 @@ namespace Learntendo_backend.Dtos
 {
     public class ChatMessageDto
     {
-        [Required]
+        
         public string SenderId { get; set; }
 
-        [Required]
+        
         public string ReceiverId { get; set; }
 
-        [Required]
+        
         public string Content { get; set; }
+        public DateTime SentAt { get; set; }
     }
 }
