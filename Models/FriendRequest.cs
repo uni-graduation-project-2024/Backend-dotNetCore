6fbe6ec87fd2e60ecb84aad439f164ae5f29bb48
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-12 19:58:32 +0300

    update model

--- Models/FriendRequest.cs
+++ Models/FriendRequest.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
 
-        public int SenderId { get; set; }
+        public int? SenderId { get; set; }
         public User Sender { get; set; }
 
-        public int ReceiverId { get; set; }
+        public int? ReceiverId { get; set; }
         public User Receiver { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public FriendRequestStatus Status { get; set; }
